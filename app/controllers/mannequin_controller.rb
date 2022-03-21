00dadd2086cddf3437243f9822491e9d4d6ddf28
kirua <yo44prg@icloud.com>
2022-03-21 22:23:56 +0100

    feat: manage image response

--- app/controllers/mannequin_controller.rb
+++ app/controllers/mannequin_controller.rb
@@ -6,7 +6,7 @@ class MannequinController < ApplicationController
       model: Model.find(model_params[:id]),
       model_infos: ModelInfo.select(:id, :size, :weight, :chest, :waist, 
         :hips, :shoe_size, :color, :hair_color, :astrological, :description).find_by(model_uuid: model_params[:id]),
-      model_picture: ModelPicture.select(:id, :picture_path, :main_picture).find_by(model_uuid: model_params[:id])
+      model_pictures: ModelPicture.select(:id, :picture_path, :main_picture).where(model_uuid: model_params[:id])
     }
     render json: res
   end
