ae38fa0c358d5cfed713409786ebecc4b46862c3
kirua <yo44prg@icloud.com>
2022-09-23 00:13:34 +0200

    chore(diag): add command for diag.sh

--- diag.sh
+++ diag.sh
@@ -1,11 +1,18 @@
 day=$( date +%d )
 env="$(rails runner "puts Rails.env")"
+noday="$1"
+
 echo "$env"
 
-# TODO: change day
-if [ "$day" = "27" ] || [ "$day" = "28" ]; then
+launch_comand () {
   rake model_without_pictures_diag DIAG_ENV="$env"
   rake picture_without_cloudinary_id_diag DIAG_ENV="$env"
   rake model_without_main_picture_diag DIAG_ENV="$env"
   rake picture_not_delete_in_cloudinary_diag DIAG_ENV="$env"
+}
+
+if [ "$noday" = "--no-day" ]; then
+  launch_comand
+elif [ "$day" = "1" ] || [ "$day" = "15" ]; then
+  launch_comand
 fi
