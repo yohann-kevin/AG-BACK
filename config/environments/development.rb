04fcdba225311183b399874a9005da0ca95b64f2
yohann perriguey <yo44prg@icloud.com>
2022-10-14 18:10:16 +0200

    chore(development): change smtp config in development mode

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = true
 
-  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.delivery_method = :smtp
   host = "localhost:3000"
   config.action_mailer.default_url_options = { host: host }
   config.action_mailer.smtp_settings = {
